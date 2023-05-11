9f19ef8c98f10b8ca0db4f73d0deeb759fa2b1fa
nikolabuca <radojkovic.nikola.gaf@gmail.com>
2023-05-11 11:38:06 +0200

    gmail route into server.js

--- controllers/gmail/GmailController.js
+++ controllers/gmail/GmailController.js
@@ -3,7 +3,7 @@ const GmailModel = require('../../models/gmail/GmailModel');
 
 function sendGmail(from, mailto, subject, text, html, user, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN){
     const mailOptions = {
-        from: `${from} <reports@zingley.com>`,
+        from: `${from} <${from}>`,
         to: mailto,
         subject,
         text,
